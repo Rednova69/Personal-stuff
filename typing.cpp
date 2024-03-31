06910febd4fe72fef0c20b09d6abc5f05c38b5c1
Red NoVa <97530785+Rednova69@users.noreply.github.com>
2024-03-30 21:46:08 -0400

    Update typing.cpp

--- typing.cpp
+++ typing.cpp
@@ -2,16 +2,15 @@
 #include <chrono>
 #include <thread>
 
-std::ostream& typewrite(std::ostream &out,
+std::mt19937_64 eng{std::random_device{}()};
+std::uniform_int_distribution<> dist{200, 600};
 
-const std::string &message)
-
+std::ostream& typewrite(std::ostream &out, const std::string &message)
 {
     for (char ch : message)
     {
         out << ch << std::flush;
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(400));
+        std::this_thread::sleep_for(std::chrono::milliseconds{dist(eng)});
     }
     out << '\n';
     return out;
